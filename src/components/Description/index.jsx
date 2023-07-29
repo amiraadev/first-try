55adeb55ebf9ad6e70ca9a00cbde703c0540115a
amirticha <allagui_fawzi@outlook.fr>
2023-07-29 21:34:35 +0100

    5

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -5,7 +5,7 @@ import { slideUp, opacity } from './animation';
 import Rounded from '../../common/RoundedButton';
 export default function index() {
 
-    const phrase = "Helping brands to stand out in the digital era. Together we will set the new status quo. No nonsense, always on the cutting edge.";
+    const phrase = "With a robust background as a former software engineer in the military, I bring a wealth of technical expertise, disciplined work ethic, and a proven track record of delivering mission-critical solutions. From front-end development to seamless back-end integration, my dedication lies in crafting secure and indispensable applications tailored to meet specific and vital requirements.";
     const description = useRef(null);
     const isInView = useInView(description)
     return (
--- src/components/Projects/components/project/index.jsx
+++ src/components/Projects/components/project/index.jsx
@@ -7,7 +7,7 @@ export default function index({index, title, manageModal}) {
     return (
         <div onMouseEnter={(e) => {manageModal(true, index, e.clientX, e.clientY)}} onMouseLeave={(e) => {manageModal(false, index, e.clientX, e.clientY)}} className="project">
             <h2>{title}</h2>
-            <p>Design & Development</p>
+            <p>Software Engineer</p>
         </div>
     )
 }
