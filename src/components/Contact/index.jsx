1f189ab0abad19e03d4efc208b8503328f9786da
amirticha <allagui_fawzi@outlook.fr>
2023-07-30 21:19:38 +0100

    03

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -4,7 +4,6 @@ import Rounded from '../../common/RoundedButton';
 import { useRef } from 'react';
 import { useScroll, motion, useTransform, useSpring } from 'framer-motion';
 import Magnetic from '../../common/Magnetic';
-import Email from '../Email';
 
 export default function index() {
     const container = useRef(null);
@@ -32,17 +31,21 @@ export default function index() {
                     <h2>together</h2>
                     <motion.div style={{x}} className="buttonContainer">
                         <Rounded  backgroundColor={"#334BD3"} className="button">
+                            <a href='/Email'>
                             <p>Get in touch</p>
+                            </a>          
                         </Rounded>
                     </motion.div>
                     <motion.svg style={{rotate, scale: 2}} width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
                     </motion.svg>
                 </div>
-                <Email/>
                 <div className="nav">
                         <Rounded>
+                        <a href='/Email'>
                             <p>allagui_amira@yahoo.com</p>
+                        </a>  
+
                         </Rounded>
                         <Rounded>
                             <p>+216 50 698 639</p>
