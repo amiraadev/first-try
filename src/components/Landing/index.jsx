3b08efe114a70932eed88e0d192b92102db77751
amirticha <allagui_fawzi@outlook.fr>
2023-08-08 21:41:42 +0100

    2

--- src/components/Landing/index.jsx
+++ src/components/Landing/index.jsx
@@ -54,16 +54,12 @@ export default function Home() {
       </div>
       <div className="sliderContainer">
         <div ref={slider} className="slider">
-          <p ref={firstText}>Freelance Developer -</p>
-          <p ref={secondText}>Freelance Developer -</p>
+          <p ref={firstText}>Software Engineer -</p>
+          <p ref={secondText}>Software Engineer -</p>
         </div>
       </div>
       <div data-scroll data-scroll-speed={0.1} className="description">
-        <svg width="9" height="9" viewBox="0 0 9 9" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z" fill="white"/>
-        </svg>
-        <p>Freelance</p>
-        <p>Designer & Developer</p>
+      
       </div>
     </motion.main>
   )
